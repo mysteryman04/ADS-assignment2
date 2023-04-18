06073bdcf24dbd70176999ccde3454f897499f50
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-18 20:35:59 +0600

    the same as in the indexOf but reversely

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -78,7 +78,12 @@ public class MyArrayList<T> implements MyList<T> {
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (arr[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
