a629433226b33229ca0cb932fb701a7612a2e368
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-01 16:33:19 +0600

    creating pop method that is implemented from MyArrayList. Remove method of stackList is called to remove the element at the last index of the array list, which represents the top of the stack, and return it.

--- src/Main_ArrayStack.java
+++ src/Main_ArrayStack.java
@@ -5,6 +5,6 @@ public class Main_ArrayStack {
         stack.push(1);
         stack.push(2);
         stack.push(3);
-
+        System.out.println(stack.pop());
     }
 }
--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -8,7 +8,12 @@ public class MyArrayListStack<T> {
     public void push(T item) {
         stackList.add(item);
     }
-
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stackList.remove(stackList.size() - 1);
+    }
     public boolean isEmpty() {
         return stackList.isEmpty();
     }
