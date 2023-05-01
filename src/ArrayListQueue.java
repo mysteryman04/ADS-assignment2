7c93ae5799c23bb162e3ecbfd928a1420b063337
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-01 22:56:51 +0600

    Creating main method in Main class. Adding some elements to the queue. Implementation of the MyQueue. Finally, enqueue method is extending add method  from MyArrayList and running successfully.

--- src/ArrayListQueue.java
+++ src/ArrayListQueue.java
@@ -1,9 +1,32 @@
 import java.util.NoSuchElementException;
-public class ArrayListQueue<T> {
+public class ArrayListQueue<T> implements MyQueue{
     private MyArrayList<T> list;
 
     public ArrayListQueue() {
         list = new MyArrayList<>();
     }
+    @Override
+    public void enqueue(Object element) {
+        list.add((T) element);
+    }
 
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- src/Main_ArrayQueue.java
+++ src/Main_ArrayQueue.java
@@ -1,2 +1,9 @@
 public class Main_ArrayQueue {
+    public static void main(String[] args) {
+        ArrayListQueue<Integer> queue = new ArrayListQueue<>();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+    }
 }
