6e71a8f25bcd4cc3cf450504e7eebcefe8e97ae0
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-01 19:17:59 +0600

    generating new java files, defining classes, constructor

---
+++ src/LinkedListQueue.java
@@ -0,0 +1,10 @@
+import java.util.NoSuchElementException;
+
+public class LinkedListQueue<T> {
+    private MyLinkedList<T> list;
+
+    public LinkedListQueue() {
+        list = new MyLinkedList<>();
+    }
+
+}
