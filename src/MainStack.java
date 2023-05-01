3a5f3b311558c20490764a5cb923d2c07ff8d78e
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-01 12:44:44 +0600

    creating for MyLinkedListStack private Main and pushing some elements onto the stack

---
+++ src/MainStack.java
@@ -0,0 +1,11 @@
+public class MainStack {
+    public static void main(String[] args) {
+        MyLinkedListStack<Integer> stack = new MyLinkedListStack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+    }
+
+}
