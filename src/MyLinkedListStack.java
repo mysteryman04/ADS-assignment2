deb726250533aaed0bf2eae7ca34cb44e1134c2e
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-01 11:23:37 +0600

    creating MyLinkedListStack constructor

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -3,6 +3,9 @@ import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
     private MyLinkedList<T> stackList;
 
+    public MyLinkedListStack() {
+        stackList = new MyLinkedList<>();
+    }
 
 
 }
